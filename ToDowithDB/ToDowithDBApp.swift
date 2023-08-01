b2670306f0ae74b8ede40cb28dec61669063507c
Naomi <naomipremlim@gmail.com>
2023-08-01 02:15:23 -0700

    Initial Commit

---
+++ ToDowithDB/ToDowithDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDowithDBApp.swift
+//  ToDowithDB
+//
+//  Created by Naomi on 1/8/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDowithDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
